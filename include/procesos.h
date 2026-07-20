1faf2e4b90a749f12ea7c02df30d69fe769304d0
Fabiana Paola Rojas Condori <frojascon@unsa.edu.pe>
2026-07-19 22:23:56 -0500

    Archivo de cabecera 'procesos.h' para el módulo de procesos

--- include/procesos.h
+++ include/procesos.h
@@ -1,1 +1,13 @@
+#ifndef PROCESOS_H
+#define PROCESOS_H
 
+void menuProcesos();
+void listarProcesos();
+void buscarProceso();
+void mostrarCPUyMemoria();
+void finalizarProceso();
+void suspenderProceso();
+void reanudarProceso();
+void arbolProcesos();
+
+#endif
